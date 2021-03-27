c421c15d31283c4168ffa04bc3fe7fe04e1e158f
Alejandro Gil <ggilmay@eafit.edu.co>
2021-03-26 20:17:57 -0500

    Cambio en la aplicacion

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido!");
+        return String.format("Bienvenido! probando app");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
